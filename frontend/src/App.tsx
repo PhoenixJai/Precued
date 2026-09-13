6cde467617e7cd55138a8a82eb1b3b4535de7188
Jaiden Wilson <93148081+PhoenixJai@users.noreply.github.com>
2026-09-13 09:54:20 -0400

    feat: route guest joins by invite token

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,7 @@ export default function App() {
       <Route path="/login" element={<LoginPage />} />
       <Route path="/profile" element={<ProfilePage />} />
       <Route path="/join" element={<GuestJoinPage />} />
-      <Route path="/join/:roomId/:roomRoleId" element={<GuestJoinPage />} />
+      <Route path="/join/:inviteToken" element={<GuestJoinPage />} />
       <Route path="/templates" element={<TemplatePickerPage />} />
       <Route path="/templates/custom/new" element={<CustomTemplateBuilderPage />} />
       <Route path="/templates/custom/:templateId" element={<CustomTemplateBuilderPage />} />
